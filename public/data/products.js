73995fa311502ec2eaf5e8f0ab0504ce0f974032
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-10-05 23:28:23 +0700

    feat: correct data

---
+++ public/data/products.js
@@ -0,0 +1,66 @@
+export const products = [
+  {
+    id: 1,
+    image: "/public/images/coffeHazelnut.png",
+    title: "Hazelnut Latte",
+    description:
+      "You can explore the menu that we provide with fun and have their own taste and make your day better.",
+    price: 20000,
+  },
+  {
+    id: 2,
+    image: "/public/images/coffeHazel2.png",
+    title: "Hazelnut Latte",
+    description:
+      "You can explore the menu that we provide with fun and have their own taste and make your day better.",
+    price: 20000,
+  },
+  {
+    id: 3,
+    image: "/public/images/coffeHazel3.png",
+    title: "Hazelnut Latte",
+    description:
+      "You can explore the menu that we provide with fun and have their own taste and make your day better.",
+    price: 20000,
+  },
+  {
+    id: 4,
+    image: "/public/images/coffeHazel4.jpg",
+    title: "Hazelnut Latte",
+    description:
+      "You can explore the menu that we provide with fun and have their own taste and make your day better.",
+    price: 20000,
+  },
+];
+
+export const ourProductCard = [
+  {
+    id: 1,
+    image: "/public/images/iconWomen.png",
+    title: "HAPPY MOTHER'S DAY!",
+    description: "Get one of our favorite menu for free!",
+    ticket: "Klaim kupon",
+  },
+  {
+    id: 2,
+    image: "/public/images/iconWomen.png",
+    title: "HAPPY MOTHER'S DAY!",
+    description: "Get one of our favorite menu for free!",
+    ticket: "Klaim kupon",
+  },
+  {
+    id: 3,
+    image: "/public/images/iconWomen.png",
+    title: "HAPPY MOTHER'S DAY!",
+    description: "Get one of our favorite menu for free!",
+    ticket: "Klaim kupon",
+  },
+  {
+    id: 4,
+    image: "/public/images/image.png",
+    title: "Get a cup of coffee for free on sunday morning ",
+    description: "Only at 7 to 9 AM",
+    ticket: "",
+    bgColor: "bg-[##F5C361]",
+  },
+];
