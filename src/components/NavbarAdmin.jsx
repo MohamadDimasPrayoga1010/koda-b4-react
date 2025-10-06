cde1f2f916203d8abfbaa6b86a7f16a490691478
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-10-06 23:21:29 +0700

    feat: create a navbar for the admin dashboard

---
+++ src/components/NavbarAdmin.jsx
@@ -0,0 +1,42 @@
+
+import CoffeLogo from "/images/CoffeLogo.png";
+import { Link} from "react-router-dom";
+import SearchIcon from "/images/Search.png";
+import CartIcon from "/images/ShoppingCart.png";
+import { X, Menu, Search, ShoppingCart, ChevronDown } from "lucide-react";
+import profile from "/images/coffeHazel4.jpg";
+
+const NavbarAdmin = () => {
+  return (
+    <>
+      <header className="fixed top-0 left-0 w-full z-50  bg-[#E8E8E8]/80  py-5 px-6 md:px-16 ">
+        <nav className="flex justify-between items-center">
+          <div className="flex items-center gap-3 md:gap-5">
+            <img src={CoffeLogo} alt="coffe-logo" className="h-8 md:h-10" />
+          </div>
+
+          <div className="hidden md:flex items-center gap-4">
+            <Link>
+              <Search className="w-5 h-5" />
+            </Link>
+            <Link>
+              <ShoppingCart className="w-5 h-5" />
+            </Link>
+            <Link to="">
+              <img
+                src={profile}
+                alt="profile-img"
+                className="w-9 h-9 object-cover rounded-full"
+              />
+            </Link>
+            <Link to="">
+              <ChevronDown className="w-5 h-5 text-[#4F5665]" />
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default NavbarAdmin;
