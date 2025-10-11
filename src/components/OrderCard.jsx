bd282f50f2e683ef3e2533da76db3cef5676e8e7
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-10-12 00:17:38 +0700

    feat: Creating components for cardorder

---
+++ src/components/OrderCard.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { CalendarDays, Repeat } from "lucide-react";
+import { Link } from "react-router-dom";
+import Glass from "/images/glass.png";
+import RepeatImg from "/images/repeat.png";
+
+const OrderCard = ({ order }) => {
+  return (
+    <div key={order.id} className="my-4 flex gap-3">
+      <img
+        src={order.items?.[0]?.image || "/images/coffeHazel4.jpg"}
+        alt="order-img"
+        className="w-[111px] h-[105px] object-cover rounded-md hidden md:flex"
+      />
+
+      <div className="flex flex-col bg-[#E8E8E84D] p-4 rounded-md flex-1">
+        <div className="grid gap-2 grid-cols-2 md:grid-cols-4">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-2 text-base text-[#4F5665]">
+              <img src={Glass} alt="glass-icon" className="w-5 h-5" />
+              <span>No. Order</span>
+            </div>
+            <span className="text-base font-bold">{order.orderId}</span>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="flex items-center gap-2 text-base text-[#4F5665]">
+              <CalendarDays className="w-5 h-5" />
+              <span>Date</span>
+            </p>
+            <span className="text-base font-bold">{order.date}</span>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="flex items-center gap-2 text-base text-[#4F5665]">
+              <Repeat className="w-5 h-5" />
+              <span>Total</span>
+            </p>
+            <span className="text-base font-bold">
+              IDR {Math.round(order.total).toLocaleString("id-ID")}
+            </span>
+          </div>
+
+          <div className="flex flex-col items-start">
+            <div className="flex items-center gap-2 text-base text-[#4F5665]">
+              <img src={RepeatImg} alt="repeat-icon" className="w-5 h-5" />
+              <span>Status</span>
+            </div>
+            <button
+              className={`py-2 px-3 rounded-2xl ${order.statusColor} font-semibold`}
+            >
+              {order.status}
+            </button>
+          </div>
+        </div>
+
+        <Link
+          to={`/order-detail/${order.id}`}
+          className="text-[#FF8906] text-sm font-semibold mt-3 hover:underline"
+        >
+          View Order Detail
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCard;
