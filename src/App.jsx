2851a241a365edc1a55e93641492fd62316607c8
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-10-13 08:48:26 +0700

    feat: add navigation for admin

--- src/App.jsx
+++ src/App.jsx
@@ -12,73 +12,51 @@ import OrderDetail from "./pages/OrderDetail";
 import Profile from "./pages/Profile";
 import MainLayoutAdmin from "./layout/MainLayoutAdmin";
 import Dashboard from "./pages/Dashboard";
-import { AuthProvider } from "./context/AuthContext"; 
+import { AuthProvider } from "./context/AuthContext";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import ProductList from "./pages/ProductList";
+import OrderList from "./pages/OrderList";
+import UserList from "./pages/UserList";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-      {
-        path: "/",
-        element: <HomaPage />,
-      },
-      {
-        path: "/our-product",
-        element: <OurProduct />,
-      },
-      {
-        path: "/detail-product/:slug",
-        element: <DetailProduct />,
-      },
-      {
-        path: "/payment-details",
-        element: <PaymentDetails />,
-      },
-      {
-        path: "/history-order",
-        element: <HistoryOrder />,
-      },
-      {
-        path: "/order-detail/:id",
-        element: <OrderDetail />,
-      },
-      {
-        path: "profile",
-        element: <Profile />,
-      },
-    ],
-  },
-
-  {
-    path: "/",
-    element: <MainLayoutAdmin />,
-    children: [
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
+      { path: "/", element: <HomaPage /> },
+      { path: "/our-product", element: <OurProduct /> },
+      { path: "/detail-product/:slug", element: <DetailProduct /> },
+      { path: "/payment-details", element: <PaymentDetails /> },
+      { path: "/history-order", element: <HistoryOrder /> },
+      { path: "/order-detail/:id", element: <OrderDetail /> },
+      { path: "profile", element: <Profile /> },
     ],
   },
   {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/forgot-password",
-    element: <ForgotPassword />,
-  },
+  path: "/",
+  element: <MainLayoutAdmin />,
+  children: [
+    { path: "/dashboard", element: <Dashboard /> },
+  {path:"/product-list" ,element: <ProductList />},
+{path: "/order-list", element: <OrderList />},
+{path: "/user-list", element: <UserList />}],
+},
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/forgot-password", element: <ForgotPassword /> },
 ]);
 
 function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </PersistGate>
+    </Provider>
   );
 }
 
