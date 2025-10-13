bdd0879a8179599d35ca101f9e6ffe28235dc3cb
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-10-13 08:44:45 +0700

    feat: add main layout structure with navbar and sidebar

--- src/layout/MainLayoutAdmin.jsx
+++ src/layout/MainLayoutAdmin.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import NavbarAdmin from '../components/NavbarAdmin'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
 
 const MainLayoutAdmin = () => {
   return (
-    <div>
-      <NavbarAdmin />
-      <Outlet />
+    <div className="flex h-screen bg-gray-50">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <NavbarAdmin />
+        <main className="flex-1 overflow-auto p-8 mt-20">
+          <Outlet />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
 export default MainLayoutAdmin
