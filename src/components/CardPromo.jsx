785f6a23b02a107ccf0b3e33a6f698032d4cba43
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-10-13 22:27:24 +0700

    feat: added jsdocs and fixed pagination

--- src/components/CardPromo.jsx
+++ src/components/CardPromo.jsx
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from "react";
 import { MoveLeft, MoveRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
+/**
+ * @typedef {Object} Promo
+ * @property {number} id - ID unik untuk promo.
+ * @property {string} title - Judul promo.
+ * @property {string} description - Deskripsi singkat promo.
+ * @property {string} image - URL gambar promo.
+ * @property {string} [ticket] - Keterangan tambahan promo (misalnya kode voucher).
+ * @property {string} [bgColor] - Warna latar belakang promo (contoh: "bg-[#88B788]").
+ */
+
 const PromoSection = () => {
   const [promos, setPromos] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
@@ -36,12 +46,12 @@ const PromoSection = () => {
   );
 
   return (
-    <section className="my-10 mx-6 md:mx-12">
+    <section className="my-10">
       <div className="flex justify-between items-center">
-        <h3 className="text-3xl md:text-5xl font-medium">
+        <h3 className="text-3xl mx-12 md:text-5xl font-medium">
           Today <span className="text-[#8E6447]">Promo</span>
         </h3>
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center mx-12">
           <button
             onClick={handlePrev}
             className="bg-[#E8E8E8] p-3 rounded-full hover:bg-[#D6D6D6] transition"
@@ -57,11 +67,11 @@ const PromoSection = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+      <div className="flex overflow-x-auto gap-8 mt-8 hide-scrollbar ">
         {displayedPromos.map((promo) => (
           <div
             key={promo.id}
-            className={`rounded-xl p-5 flex items-center gap-4 text-white shadow-md ${
+            className={`rounded-xl flex flex-nowrap  items-center gap-4 text-white min-w-sm md:min-w-xs shadow-md ${
               promo.bgColor ? promo.bgColor : "bg-[#88B788]"
             }`}
           >
@@ -80,6 +90,18 @@ const PromoSection = () => {
               )}
             </div>
           </div>
+        ))}
+      </div>
+      <div className="flex gap-2 mt-4 mx-12 md:justify-start">
+        {Array.from({ length: totalPages }).map((_, idx) => (
+          <span
+            key={idx}
+            className={`transition-all duration-300 ${
+              idx === currentPage
+                ? "bg-[#FF8906] w-6 h-3 rounded-full"
+                : "bg-gray-300 w-3 h-3 rounded-full"
+            }`}
+          />
         ))}
       </div>
     </section>
