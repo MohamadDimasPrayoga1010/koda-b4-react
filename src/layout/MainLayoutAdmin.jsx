c67be307829426f25d4f89db3b262c15036efa76
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-12-06 10:26:18 +0700

    refactor: fix ui

--- src/layout/MainLayoutAdmin.jsx
+++ src/layout/MainLayoutAdmin.jsx
@@ -6,14 +6,18 @@ import Sidebar from '../components/Sidebar';
 const MainLayoutAdmin = () => {
   return (
     <div className="flex h-screen bg-gray-50">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <NavbarAdmin />
-        <main className="flex-1 overflow-auto p-8 mt-20">
-          <Outlet />
-        </main>
-      </div>
-    </div>
+  <div className="w-64 h-full bg-white shadow-md">
+    <Sidebar />
+  </div>
+
+  <div className="flex-1 flex flex-col h-full">
+    <NavbarAdmin />
+    <main className="flex-1 overflow-auto p-8 mt-20">
+      <Outlet />
+    </main>
+  </div>
+</div>
+
   );
 }
 
